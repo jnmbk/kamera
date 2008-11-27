0c580ddfd7c0f7b35bfd3c97aa0d579747130069
Uğur Çetin <ugur@ugurcetin.com.tr>
2008-11-27 08:08:38 +0000

    update translation

--- data/kamera_tr_TR.ts
+++ data/kamera_tr_TR.ts
@@ -2,6 +2,104 @@
 <!DOCTYPE TS><TS version="1.1" language="tr">
 <defaultcodec></defaultcodec>
 <context>
+    <name>ConfigWindow</name>
+    <message>
+        <location filename="configwindow.ui" line="13"/>
+        <source>Settings - Kamera</source>
+        <translation>Tercihler - Kamera</translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="27"/>
+        <source>Image</source>
+        <translation>Fotoğraf</translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="37"/>
+        <source>Saving Direcory</source>
+        <translation>Kaydetme Dizini</translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="47"/>
+        <source>Format</source>
+        <translation>Biçim</translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="70"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="83"/>
+        <source>bmp</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="88"/>
+        <source>jpg</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="93"/>
+        <source>png</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="98"/>
+        <source>ppm</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="103"/>
+        <source>tiff</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="108"/>
+        <source>xbm</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="113"/>
+        <source>xpm</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="139"/>
+        <source>Video</source>
+        <translation>Görüntü</translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="149"/>
+        <source>Webcam</source>
+        <translation>Web Kamerası</translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="160"/>
+        <source>Default</source>
+        <translation>Öntanımlı</translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="170"/>
+        <source>Flipping</source>
+        <translation>Çevir</translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="176"/>
+        <source>Flip Horizontally</source>
+        <translation>Yatay Çevir</translation>
+    </message>
+    <message>
+        <location filename="configwindow.ui" line="183"/>
+        <source>Flip Vertically</source>
+        <translation>Dikey Çevir</translation>
+    </message>
+    <message>
+        <location filename="configwindow.py" line="51"/>
+        <source>Select Image Directory</source>
+        <translation>Fotoğraf Dizinini Seçin</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="13"/>
@@ -9,37 +107,37 @@
         <translation>Kamera</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="43"/>
+        <location filename="mainwindow.ui" line="47"/>
         <source>Getting video from webcam</source>
         <translation>Web kamerasından görüntü alınıyor</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="70"/>
+        <location filename="mainwindow.ui" line="74"/>
         <source>&amp;Save</source>
         <translation>&amp;Kaydet</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="128"/>
         <source>&amp;Help</source>
-        <translation>&amp;Yardım</translation>
+        <translation type="obsolete">&amp;Yardım</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="137"/>
         <source>About &amp;Qt...</source>
-        <translation>&amp;Qt Hakkında...</translation>
+        <translation type="obsolete">&amp;Qt Hakkında...</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="142"/>
         <source>About &amp;Kamera...</source>
-        <translation>&amp;Kamera Hakkında...</translation>
+        <translation type="obsolete">&amp;Kamera Hakkında...</translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="70"/>
+        <location filename="mainwindow.py" line="89"/>
         <source>About Kamera</source>
         <translation>Kamera Hakkında</translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="70"/>
+        <location filename="mainwindow.py" line="89"/>
         <source>Kamera %1 - webcam photographer
 This software is released under the terms of GPL v3.
 http://kamera.googlecode.com
@@ -52,6 +150,16 @@ http://kamera.googlecode.com￼
 
 ￼Geliştirici:￼
 Uğur Çetin &lt;ugur.jnmbk at gmail.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="81"/>
+        <source>&amp;Configure...</source>
+        <translation>&amp;Yapılandır...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="88"/>
+        <source>&amp;Aboout...</source>
+        <translation>&amp;Hakkında...</translation>
     </message>
 </context>
 </TS>
